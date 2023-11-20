ba7ca60a736162fd1000295fb8a5cd10c8897973
Alexandr150983 <alexandr150983@gmail.com>
2023-11-20 14:31:59 +0200

    fix bag

--- src/generic/6.ts
+++ src/generic/6.ts
@@ -7,16 +7,14 @@
   Виправте тип у аргументі функції так, щоб не було помилок типу.
 */
 
-type User = Partial<{
+type User = {
   name: string;
   surname: string;
   email: string;
   password: string;
-}>;
+};
 
-function createOrUpdateUser(initialValues: User) {
-  // Оновлення користувача
-}
+function createOrUpdateUser(initialValues: Partial<User>) {}
 
 createOrUpdateUser({ email: "user@mail.com", password: "password123" });
 
